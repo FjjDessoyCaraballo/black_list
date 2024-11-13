f107e6fbe0656056c4afa4a4115a5b6336082006
fjjdessoycaraballo <fdessoycaraballo@gmail.com>
2024-11-13 15:10:35 +0200

    feature: adding executable that will scour the zsh_history file

---
+++ Makefile
@@ -0,0 +1,34 @@
+NAME		=	bl
+
+COMP		=	gcc
+FLAGS		=	-Wall -Wextra -Werror -g -pedantic -fsanitize=address
+
+SRC_DIR		=	src
+OBJ_DIR		=	obj
+
+SRCS 		= $(SRC_DIR)/main.c\
+
+OBJS 		= $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRCS))
+
+all: $(NAME)
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c | $(OBJ_DIR)
+	@$(COMP) $(FLAGS) -c $< -o $@
+
+$(NAME): $(OBJS)
+	@$(COMP) $(FLAGS) $(OBJS) -o $(NAME)
+
+$(OBJ_DIR):
+	@mkdir -p $(OBJ_DIR)
+
+clean:
+	@rm -rf $(OBJS)
+	@echo "cleaning objects"
+
+fclean: clean
+	@rm -rf $(NAME)
+	@echo "removing $(NAME)"
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ src/main.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*  ROFL:ROFL:ROFL:ROFL 													* */
+/*          _^___      														* */
+/* L     __/   [] \    														* */
+/* LOL===__        \   			MY ROFLCOPTER GOES BRRRRRR					* */
+/* L      \________]  					by fdessoy-							* */
+/*         I   I     			(fdessoy-@student.hive.fi)					* */
+/*        --------/   														* */
+/* ************************************************************************** */
+
+#include "../inc/blackList.h"
+
+const char* black_list[] = {"curl", "wget", "lynx", "links", "nc", "telnet", "ftp", "scp", "rsync", "git clone"};
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		static t_data *data;
+		data = calloc(1, sizeof(data));
+		if (!data)
+		{
+			write(1, "Failure to allocate\n", 21);
+			return (1);
+		}
+		int option = atoi(argv[1]);
+		switch (option)
+		{
+			case 1:
+				write(1, "ok\n", 3);
+				break ;
+			case 2:
+				write(1, "option 2\n", 10);
+				break ;
+			case 3:
+				write(1, "option 3\n", 10);
+				break ;
+			case 4:
+				write(1, "option 4\n", 10);
+				break ;
+			default:
+				write(1, ERROR1, strlen(ERROR1));
+				break ;
+		}
+	}
+	else
+	{
+		write(1, ERROR1, strlen(ERROR1));
+		return (FAILURE);
+	}
+	return (0);
+}
